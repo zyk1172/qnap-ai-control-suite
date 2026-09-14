175203fdaa62cefba36678aecf24c9de29f9c571
zyk1172 <53221608+zyk1172@users.noreply.github.com>
2026-09-14 13:38:57 +0800

    test: fix contract helper imports

--- mac-bridge/test/contracts.test.js
+++ mac-bridge/test/contracts.test.js
@@ -2,7 +2,6 @@ import assert from "node:assert/strict";
 import test from "node:test";
 
 import {
-  normalizeQPKGOperation,
   normalizeStorageCommandResult,
   normalizeStorageSettledResult,
   normalizeToolOutput,
